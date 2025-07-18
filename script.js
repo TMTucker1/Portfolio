d6ff92980e3b246b45975784db8de6f1334cdc1a
Tiffany M Tucker <tmharris1986@gmail.com>
2025-07-18 01:59:38 +0000

    Implement dynamic loading of recent projects from JSON and add hover effect for project descriptions

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+fetch('projects.json')
+  .then(response => response.json())
+  .then(projects => {
+    const container = document.getElementById('projects-container');
+    container.innerHTML = projects.map(project => `
+      <div class="recent-project-card" style="text-align: center; position: relative;">
+        <a href="${project.url}" target="_blank" class="project-link-btn">
+          ${project.title}
+        </a>
+        <p class="project-hover-desc">${project.description}</p>
+      </div>
+    `).join('');
+  });
+</script>
